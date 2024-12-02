3430e23ba0e57edaec7b80cb7e13ead1795971ed
khadar easy solutions <mohamedaliosman2001@gmail.com>
2024-12-02 09:27:22 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+let price = "lemon"
+
+switch (price) {
+    case "apple":
+        console.log("Apple price Is 5$")
+        break
+
+    case "lemon":
+        console.log("lemon price is: 20$")
+        break
+
+
+    case "banana":
+        console.log("banana price is: 30$")
+        break
+
+
+    case "juice":
+        console.log("juice price is: 10$")
+        break
+    default:
+        console.log("No")
+
+}
